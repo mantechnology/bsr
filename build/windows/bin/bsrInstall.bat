c86b8c446d9caef486aceb3833ca011b08262e22
swhan <swhan@mantech.co.kr>
2019-09-16 14:23:41 +0900

    DW-221 modify the bsr inf file and install/uninstall bat file

---
+++ build/windows/bin/bsrInstall.bat
@@ -0,0 +1,17 @@
+@echo off
+
+setlocal
+
+set DRIVER_PATH=.
+
+if not (%1)==() set DRIVER_PATH=%1
+
+cd %DRIVER_PATH%
+
+rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 128 .\bsrfsflt.inf
+sc config bsrfsflt start= boot binPath= \SystemRoot\system32\Drivers\bsrfsflt.sys
+
+rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 0 .\bsrvflt.inf
+
+
+rem echo reboot...
---
+++ build/windows/bin/bsrUninstall.bat
@@ -0,0 +1,12 @@
+@echo off
+
+setlocal
+
+set DRIVER_PATH=.
+
+if not (%1)==() set DRIVER_PATH=%1
+cd %DRIVER_PATH%
+rundll32.exe setupapi.dll,InstallHinfSection DefaultunInstall 0 .\bsrvflt.inf
+rundll32.exe setupapi.dll,InstallHinfSection DefaultunInstall 0 .\bsrfsflt.inf
+
+echo unstall finished. please reboot now.
