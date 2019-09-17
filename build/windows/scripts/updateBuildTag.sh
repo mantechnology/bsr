47e37b009cf9614ab5529bd6b27c71572532f600
swhan <swhan@mantech.co.kr>
2019-09-17 13:28:31 +0900

    BSR-230 remove drbd phrase

--- build/windows/scripts/updateBuildTag.sh
+++ build/windows/scripts/updateBuildTag.sh
@@ -2,16 +2,16 @@
 # $1 : Version
 if test -e .git && GITHEAD=$(git rev-parse HEAD); then
 	ENGVER=$1
-	sed -i "s/\(^#define BUILD_VERSION\).*/\1 \"$ENGVER\"/g" drbd/drbd_buildtag.c 
-	sed -i "s/\(^#define COMMIT\).*/\1 \"${GITHEAD:0:7}\"/g" drbd/drbd_buildtag.c
-	sed -i "s/\(^#define BUILD_USER\).*/\1 \"$(id -un)\"/g" drbd/drbd_buildtag.c
-	sed -i "s/\(^#define BUILD_HOST\).*/\1 \"$HOSTNAME\"/g" drbd/drbd_buildtag.c
-elif ! test -e drbd_buildtag.c ; then				
-	echo >&2 "drbd_buildtag.c not found.";					
+	sed -i "s/\(^#define BUILD_VERSION\).*/\1 \"$ENGVER\"/g" bsr-platform/windows/bsrvflt/bsr_windows_buildtag.c 
+	sed -i "s/\(^#define COMMIT\).*/\1 \"${GITHEAD:0:7}\"/g" bsr-platform/windows/bsrvflt/bsr_windows_buildtag.c
+	sed -i "s/\(^#define BUILD_USER\).*/\1 \"$(id -un)\"/g" bsr-platform/windows/bsrvflt/bsr_windows_buildtag.c
+	sed -i "s/\(^#define BUILD_HOST\).*/\1 \"$HOSTNAME\"/g" bsr-platform/windows/bsrvflt/bsr_windows_buildtag.c
+elif ! test -e bsr_windows_buildtag.c ; then				
+	echo >&2 "bsr_windows_buildtag.c not found.";					
 	test -e ../.git &&						
 	>&2 printf "%s\n"						
 		"git did not work, but this looks like a git checkout?"	
 		"Install git and try again." ||				
-	echo >&2 "Your WDRBD source tree is broken. Unpack again.";	
+	echo >&2 "Your BSR source tree is broken. Unpack again.";	
 	exit 1;								
 fi ;		
