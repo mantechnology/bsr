90b8c02dbd1efa2c8a5a8b54cf6117405e78b66f
swhan <swhan@mantech.co.kr>
2019-09-19 15:11:53 +0900

    BSR-197 change geric_hwieght64, hweight64 return type to ULONG_PTR (BSR-234)

--- bsr/bsr-kernel-compat/windows/hweight.h
+++ bsr/bsr-kernel-compat/windows/hweight.h
@@ -1,9 +1,10 @@
 #ifndef __HWEIGHT_H__
 #define __HWEIGHT_H__
 #include "../../../bsr-headers/windows/types.h"
+#include "../bsr_wrappers.h"
 
 extern unsigned int hweight32(unsigned int w);
 extern unsigned int hweight16(unsigned int w);
 extern unsigned int hweight8(unsigned int w);
-extern unsigned long long hweight64(__u64 w);
+extern ULONG_PTR hweight64(__u64 w);
 #endif
