a67f89f348615aca0248171c3777ab6ab357a73c
myyoo <myyoo_mantech@mantech.co.kr>
2019-09-26 13:57:42 +0900

    BSR-294 common code (DW-1237)

--- bsr/bsr_req.c
+++ bsr/bsr_req.c
@@ -36,11 +36,7 @@
 #endif
 
 // DW-1200 currently allocated request buffer size in byte.
-#ifdef _WIN32
-atomic_t64 g_total_req_buf_bytes = 0;
-#else
 atomic_t64 g_total_req_buf_bytes = ATOMIC_INIT(0);
-#endif
 
 static bool drbd_may_do_local_read(struct drbd_device *device, sector_t sector, int size);
 
