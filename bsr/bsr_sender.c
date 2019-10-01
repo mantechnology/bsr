dcb1198bf71c65238d8f02a1374fdd5af79fddaf
swhan <swhan@mantech.co.kr>
2019-10-01 15:23:23 +0900

    BSR-330 modify BSOD * referenced after releasing the bio from the log. * change log output location and set log annotation

--- bsr/bsr_sender.c
+++ bsr/bsr_sender.c
@@ -156,6 +156,9 @@ BIO_ENDIO_TYPE drbd_md_endio BIO_ENDIO_ARGS(struct bio *bio, int error)
 		IoFreeIrp(Irp);
 	}
 
+	// BSR-330
+	//drbd_debug(NO_OBJECT,"drbd_md_io_complete: md_io->done(%d) bio se:0x%llx sz:%d\n", device->md_io.done, bio->bi_sector, bio->bi_size);
+
 	if ((ULONG_PTR)DeviceObject != FAULT_TEST_FLAG) {
 		bio_put(bio);
 	}
@@ -171,12 +174,12 @@ BIO_ENDIO_TYPE drbd_md_endio BIO_ENDIO_ARGS(struct bio *bio, int error)
 	 * ASSERT(atomic_read(&mdev->md_io_in_use) == 1) there.
 	 */
 	drbd_md_put_buffer(device);
-
-    drbd_debug(NO_OBJECT,"drbd_md_io_complete: md_io->done(%d) bio se:0x%llx sz:%d\n", device->md_io.done, bio->bi_sector, bio->bi_size);
+	 
 	device->md_io.done = 1;
 	wake_up(&device->misc_wait);
 
-	drbd_debug(NO_OBJECT,"drbd_md_endio done.(%d)................!!!\n", md_endio_cnt++);
+	// BSR-330
+	//drbd_debug(NO_OBJECT, "drbd_md_endio done.(%d)................!!!\n", md_endio_cnt++);
 
 	BIO_ENDIO_FN_RETURN;
 
