c0893fe972063d2175301e51e3902db22ed24f8c
myyoo <myyoo_mantech@mantech.co.kr>
2019-10-10 13:58:09 +0900

    BSR-385 common (DW-1927)

--- bsr/bsr_int.h
+++ bsr/bsr_int.h
@@ -147,6 +147,8 @@ extern char usermode_helper[];
 #define ID_SYNCER_SPLIT (ID_SYNCER - 1)
 
 #define UUID_NEW_BM_OFFSET ((u64)0x0001000000000000ULL)
+//DW-1927
+#define CONTROL_BUFF_SIZE	1024 * 5120
 
 struct drbd_device;
 struct drbd_connection;
