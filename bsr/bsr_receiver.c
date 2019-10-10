b49a80148d694da048dcfc1d2c6b340a9b1b1c2d
swhan <swhan@mantech.co.kr>
2019-10-10 14:39:52 +0900

    BSR-356 common DW-1773

--- bsr/bsr_receiver.c
+++ bsr/bsr_receiver.c
@@ -571,13 +571,11 @@ void __drbd_free_peer_req(struct drbd_peer_request *peer_req, int is_net)
 	struct drbd_peer_device *peer_device = peer_req->peer_device;
 #ifndef _WIN32
 	might_sleep();
-#else
+#endif
 	// DW-1773 peer_request is managed as inactive_ee, so peer_req_databuf is modified to be released from drbd_free_peer_req()
-	//if ( !(peer_req->flags & EE_WRITE) && peer_req->peer_req_databuf)	
 	if (peer_req->peer_req_databuf) {
 		kfree2(peer_req->peer_req_databuf);
 	}
-#endif
 
 	if (peer_req->flags & EE_HAS_DIGEST)
 		kfree(peer_req->digest);
