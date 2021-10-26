028affd5b3992fad84b09e30cc2da20cd8df040e
sachoi <sachoi@mantech.co.kr>
2021-10-26 13:53:36 +0900

    BSR-786 fix ubuntu 21.10(v5.13) build error: add COMPAT_VFS_MKDIR_HAS_NS_PARAMS

--- bsr/bsr-kernel-compat/tests/have_kern_path_create.c
+++ bsr/bsr-kernel-compat/tests/have_kern_path_create.c
@@ -1,3 +1,4 @@
+/* { "version": "3.1-rc1", "commit": "dae6ad8f37529963ae7df52baaccf056b38f210e", "comment": "new helpers: kern_path_create/user_path_create" */
 #include <linux/fs.h>
 #include <linux/namei.h>
 
