7945c7363a47fab12a01e6fa982c6d69d9e5855f
sachoi <sachoi@mantech.co.kr>
2022-02-22 15:23:01 +0900

    prepare 1.6.1-A8

--- bsr/linux/bsr_config.h
+++ bsr/linux/bsr_config.h
@@ -32,8 +32,8 @@ extern const char *bsr_buildtag(void);
 
 /* End of external module for 2.6.33 stuff */
 
-#define TAG_VERSION "1.6.1(2022.1.19_A7)"
-#define REL_VERSION "1.6.1-A7"
+#define TAG_VERSION "1.6.1(2022.2.22_A8)"
+#define REL_VERSION "1.6.1-A8"
 #define PRO_VERSION_MIN 86
 // DW-1293 protocol version 112 starts to support fast invalidate(remote)
 // DW-1601 protocol version 113 remove association to act_log and resync_lru
