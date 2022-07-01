0ad377b4e3592ad19af1e22bf2148e85f40ead05
한성원 <swhan@mantech.co.kr>
2022-07-01 15:01:26 +0900

    BSR-896 restores invalid file fixes

--- bsr-utils/user/shared/bsr_buildtag.c
+++ bsr-utils/user/shared/bsr_buildtag.c
@@ -1,7 +1,22 @@
-/* automatically generated. DO NOT EDIT. */
+#ifdef _WIN
+#include <string.h>
+#else // _LIN
 #include <bsr.h>
+#endif
+
+/* automatically generated. DO NOT EDIT. */
+#define BUILD_VERSION ""
+#define COMMIT ""
+#define BUILD_USER ""
+#define BUILD_HOST ""
+
 const char *bsr_buildtag(void)
 {
-	return "1.6.2-A2 GIT-hash: 01c6015"
-		" build by root@p1, 2022-06-30 21:56:24";
+	if (strcmp(BUILD_VERSION, "") == 0) {
+		return "";
+	}
+	else {
+		return BUILD_VERSION " GIT-hash: " COMMIT " build by " BUILD_USER"@"BUILD_HOST", " __TIMESTAMP__;
+	}
+	
 }
