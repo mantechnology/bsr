b2151dc899379f2641ad5b83710ca581bbf93483
한성원 <swhan@mantech.co.kr>
2023-02-10 09:13:55 +0900

    BSR-1030 apply the primary driver section to the inf file

--- build/windows/bin/bsrInstall.bat
+++ build/windows/bin/bsrInstall.bat
@@ -8,10 +8,10 @@ if not (%1)==() set DRIVER_PATH=%1
 
 cd %DRIVER_PATH%
 
-rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 128 .\bsrfsflt.inf
+rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall.NTamd64 128 .\bsrfsflt.inf
 sc config bsrfsflt start= boot binPath= \SystemRoot\system32\Drivers\bsrfsflt.sys
 
-rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall 0 .\bsrvflt.inf
+rundll32.exe setupapi.dll,InstallHinfSection DefaultInstall.NTamd64 0 .\bsrvflt.inf
 
 
 rem echo reboot...
--- build/windows/bin/bsrUninstall.bat
+++ build/windows/bin/bsrUninstall.bat
@@ -6,7 +6,8 @@ set DRIVER_PATH=.
 
 if not (%1)==() set DRIVER_PATH=%1
 cd %DRIVER_PATH%
-rundll32.exe setupapi.dll,InstallHinfSection DefaultunInstall 0 .\bsrvflt.inf
-rundll32.exe setupapi.dll,InstallHinfSection DefaultunInstall 0 .\bsrfsflt.inf
+
+rundll32.exe setupapi.dll,InstallHinfSection DefaultunInstall.NTamd64 0 .\bsrvflt.inf
+rundll32.exe setupapi.dll,InstallHinfSection DefaultunInstall.NTamd64 0 .\bsrfsflt.inf
 
 echo unstall finished. please reboot now.
