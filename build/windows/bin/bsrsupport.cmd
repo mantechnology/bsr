a3787c1119e125f3b1b660917c3d505a658c8433
sachoi <sachoi@mantech.co.kr>
2023-09-22 13:07:37 +0900

    BSR-1130 add bsrsupport execution log

--- build/windows/bin/bsrsupport.cmd
+++ build/windows/bin/bsrsupport.cmd
@@ -7,7 +7,12 @@
 @rem BSR-1053
 setlocal EnableDelayedExpansion
 
-echo Check environments...
+for /f "tokens=*" %%a in ('bsrcon /get_log_path') do set BSR_LOG_DIR=%%a
+
+set bsrsupport_log="%BSR_LOG_DIR%\bsrsuupport.log"
+echo [%date%_%time%] [bsrsupport] start. > %bsrsupport_log%
+
+call :logging "[Check environments] start."
 set OLDDIR=%CD%
 set SUPPORT_HOME=%BSR_PATH%\..\support
 set OUTPUT_HOME=%SUPPORT_HOME%\%COMPUTERNAME%
@@ -26,7 +31,7 @@ for %%x in (%*) do (
 	)
 )
 if exist "%OUTPUT_HOME%" (
-	echo remove exist file
+	call :logging "remove exist file"
 	rmdir /s "%OUTPUT_HOME%"
 )
 	
@@ -37,27 +42,48 @@ if not exist "%OUTPUT_HOME%" (
 set | findstr BSR_PATH 1>NUL
 
 if not %ERRORLEVEL% EQU 0 (
-    echo BSR not installed
+    call :logging "BSR not installed"
     exit -1
 )
 
+call :logging "[Check environments] complete."
+
 if "%CORE_FILE_PATH%" == "" (
-	echo Skip collection of core file.
+	call :logging "Skip collection of core file."
 ) else (
+    call :logging "[GetCoreDumpFile] start."
 	call :GetCoreDumpFile %CORE_FILE_PATH%
+    call :logging "[GetCoreDumpFile] complete."
 )
+
+call :logging "[GetBSRInfo] start."
 call :GetBSRInfo
+call :logging "[GetBSRInfo] complete."
+
 @rem BSR-976 add option to exclude system log collection to bsrsupport
 if "%EXCLUDE_SYSLOG%" == "true" (
-	echo Skip collection of system log.
+	call :logging "Skip collection of system log."
 ) else (
+    call :logging "[GetDiskPart] start."
 	call :GetDiskPart
+    call :logging "[GetDiskPart] complete."
+    call :logging "[GetSystemInfo] start."
 	call :GetSystemInfo
+    call :logging "[GetSystemInfo] complete."
 )
+call :logging "[GetBSRStatus] start."
 call :GetBSRStatus
+call :logging "[GetBSRStatus] complete."
+call :logging "[GetBSRDebugInfo] start."
 call :GetBSRDebugInfo
-call :Archive
+call :logging "[GetBSRDebugInfo] complete."
 
+call :logging "[Archive] start."
+call :Archive
+call :logging "[Archive] complete."
+
+
+echo [%date%_%time%] [bsrsupport] complete. >> %bsrsupport_log%
 endlocal
 exit /B %ERRORLEVEL%
 
@@ -66,7 +92,7 @@ exit /B %ERRORLEVEL%
 @rem ##########################################################################################
 
 :GetCoreDumpFile
-	echo Get Core Dump file...
+	call :logging "Get Core Dump file..."
 
 	for /f "delims=" %%a IN (%1) do @set core_filename=%%~nxa
 
@@ -78,9 +104,9 @@ exit /B %ERRORLEVEL%
     "%BSR_PATH%\..\support\zip" -9j "%BSR_DIR%\%CORE_ARCHIVE_NAME%" %1
 
     if %ERRORLEVEL% EQU 0 (
-		echo core file has been compressed.
+		call :logging  "core file has been compressed."
     ) else (
-        echo core file compress failed. err(%ERRORLEVEL%)
+        call :logging  "core file compress failed. err(%ERRORLEVEL%)"
 	)
 exit /B 0
 
@@ -95,7 +121,6 @@ exit /B 0
 	echo list disk >> diskList.tmp
 	echo list volume >> volumeList.tmp
 
-
 	@rem ===================================================================
 	@rem  Diskpart's  Total Volume List 
 	@rem ===================================================================
@@ -105,6 +130,8 @@ exit /B 0
 	echo. >> "%DISKPART_DIR%\diskpartInfo.txt"
 	diskpart /s volumeList.tmp >> "%DISKPART_DIR%\diskpartInfo.txt"
 	IF EXIST volumeList.tmp DEL volumeList.tmp
+    call :logging "total volume list"
+
 
 	@rem ===================================================================
 	@rem  Diskpart's Total Disk List 
@@ -115,6 +142,7 @@ exit /B 0
 	echo. >> "%DISKPART_DIR%\diskpartInfo.txt"
 	diskpart /s diskList.tmp >> "%DISKPART_DIR%\diskpartInfo.txt"
 	IF EXIST diskList.tmp DEL diskList.tmp
+    call :logging "total disk list"
 
 	@rem ===================================================================
 	@rem  Get the total number of lines in the file(diskpartInfo.txt)
@@ -135,8 +163,7 @@ exit /B 0
 
 	for /f "tokens=2" %%b in (diskListCount.txt) do (
 		set DiskCount=%%b 
-	)
-	
+	)	
 	
 	@rem ===================================================================
 	@rem  As the number of disks, diskpart command is used
@@ -160,12 +187,13 @@ exit /B 0
 	IF EXIST diskListCount.txt DEL diskListCount.txt
 
 	echo ---Complete dispart log collection--- >> "%DISKPART_DIR%\diskpartInfo.txt"
+
+    call :logging "complete dispart log collection"
 exit /B 0
 
 :GetBSRInfo
-    echo Get BSR Information...
+    call :logging "Get BSR Information..."
     set BSR_DIR=%OUTPUT_HOME%\BSR
-    for /f "tokens=*" %%a in ('bsrcon /get_log_path') do set BSR_LOG_DIR=%%a
 
     if not exist "%BSR_DIR%" ( mkdir "%BSR_DIR%" )
     if not exist "%BSR_DIR%\etc" ( mkdir "%BSR_DIR%\etc" )
@@ -177,6 +205,7 @@ exit /B 0
     if not exist "%BSR_DIR%\bin" ( mkdir "%BSR_DIR%\bin" )
 
     xcopy "%BSR_PATH%\..\etc\*" "%BSR_DIR%\etc" /e /h /k
+    call :logging "Get bsr config"
 
     @rem BSR-1117
     xcopy "%BSR_LOG_DIR%\bsr.log*" "%BSR_DIR%\log" /k 2> nul
@@ -187,56 +216,71 @@ exit /B 0
     xcopy "%BSR_LOG_DIR%\chkdsk*.log*" "%BSR_DIR%\log" /k 2> nul
     xcopy "%BSR_LOG_DIR%\rc_start.log*" "%BSR_DIR%\log" /k 2> nul
     xcopy "%BSR_LOG_DIR%\rc_stop.log*" "%BSR_DIR%\log" /k 2> nul
+    call :logging "Get bsr log"
     if "%EXCLUDE_PERFMON%" == "true" (
-        echo Skip collection of perfmon log.
+        call :logging "Skip collection of perfmon log."
     ) else (
         xcopy "%BSR_LOG_DIR%\perfmon\*" "%BSR_DIR%\log\perfmon" /e /h /k 2> nul
+        call :logging "Get bsr perfmon log"
     )
     
 
     xcopy "%BSR_PATH%\..\bin\*.pdb" "%BSR_DIR%\bin" /e /h /k
+    call :logging "Get bsr pdb"
 exit /B 0
 
 :GetSystemInfo
-    echo Get System information...
+    call :logging "Get System information..."
     set SYSTEM_DIR=%OUTPUT_HOME%\System
 
     if not exist "%SYSTEM_DIR%" ( mkdir "%SYSTEM_DIR%" )
 
     @rem Event log
     cscript //Nologo "%BSR_PATH%\..\support\DumpEventLog.vbs" localhost Application > "%SYSTEM_DIR%\ApplicationLog.csv" 2> NUL
+    call :logging "ApplicationLog.csv"
     cscript //Nologo "%BSR_PATH%\..\support\DumpEventLog.vbs" localhost System > "%SYSTEM_DIR%\SystemLog.csv" 2> NUL
+    call :logging "SystemLog.csv"
 
     @rem System information
     systeminfo > "%SYSTEM_DIR%\systeminfo.txt"
+     call :logging "systeminfo.txt"
 
     @rem Firewall setting
     netsh advfirewall show allprofiles > "%SYSTEM_DIR%\firewallpolicy.txt"
     netsh advfirewall firewall show rule name=all verbose >> "%SYSTEM_DIR%\firewallpolicy.txt"
+    call :logging "firewallpolicy.txt"
 
     @rem Security Policy
     1>NUL secedit /export /cfg "%SYSTEM_DIR%\secPolicy.txt"
+    call :logging "secPolicy.txt"
 
     @rem Service List
     wmic service list full /format:csv > "%SYSTEM_DIR%\svcList.csv"
+    call :logging "svcList.csv"
 
 	@rem Disk List
 	wmic diskdrive > "%SYSTEM_DIR%\diskList.txt"
+    call :logging "diskList.txt"
 	
     @rem Running Process info
     tasklist /v > "%SYSTEM_DIR%\procList.txt"
+    call :logging "procList.txt"
 
     @rem Network Configuration
     ipconfig /all > "%SYSTEM_DIR%\netconf.txt"
+    call :logging "netconf.txt"
 
     @rem Device list
     cscript //Nologo "%BSR_PATH%\..\support\devlist.vbs" > "%SYSTEM_DIR%\devList.csv"
+    call :logging "devList.csv"
 
     @rem Device Driver info
     1>NUL wmic sysdriver list full /format:csv > "%SYSTEM_DIR%\drvList.csv"
+    call :logging "drvList.csv"
 
     @rem Windows'etc folder copy
     1>NUL copy /y %WINDIR%\System32\Drivers\etc\* "%SYSTEM_DIR%\System_etc"
+    call :logging "System_etc"
 
     @rem Netsh
     set NETSHRESULT=%SYSTEM_DIR%\netshresult.txt
@@ -292,32 +336,40 @@ exit /B 0
     echo ---------- WINS server ---------- >> "%NETSHRESULT%"
     netsh interface ipv4 show winsservers >> "%NETSHRESULT%"
 
+    call :logging "netshresult.txt"
+
     @rem Processes
     wmic /output:"%SYSTEM_DIR%\ProcInfo.html" process list full /format:htable
+    call :logging "ProcInfo.html"
 
     @rem etc
     route print -4 > "%SYSTEM_DIR%\route4"
+    call :logging "route4"
     route print -6 > "%SYSTEM_DIR%\route6"
+    call :logging "route6"
     netstat -na > "%SYSTEM_DIR%\netstat"
+    call :logging "netstat"
 exit /B 0
 
 :GetBSRStatus
-    echo Get BSR Status information...
+    call :logging "Get BSR Status information..."
     set STATUS_DIR=%OUTPUT_HOME%\Status
 
     if not exist "%STATUS_DIR%" ( mkdir "%STATUS_DIR%" )
 	
 	bsradm show-gi all > "%STATUS_DIR%\gi.txt"
+    call :logging "gi.txt"
     bsradm dump > "%STATUS_DIR%\dump.txt"
-	bsrsetup status --s --v all > "%STATUS_DIR%\status.txt"
+	call :logging "dump.txt"
 	bsrsetup show > "%STATUS_DIR%\show.txt"
+    call :logging "show.txt"
 exit /b 0
 
 @rem BSR-675 add debug info to support file
 :GetBSRDebugInfo
 
 setlocal EnableDelayedExpansion
-    echo Get BSR debug information...
+    call :logging "Get BSR debug information..."
     set DEBUG_DIR=%OUTPUT_HOME%\Debuginfo
 
     if not exist "%DEBUG_DIR%" ( mkdir "%DEBUG_DIR%" )
@@ -364,7 +416,7 @@ endlocal
 exit /b 0
 
 :Archive
-    echo Archive files...
+    call :logging "Archive files..."
 
     for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
     set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
@@ -378,5 +430,12 @@ exit /b 0
     cd /d "%OLDDIR%"
 
     echo.
-    echo Saved to %SUPPORT_HOME%\%ARCHIVE_NAME%
+    call :logging "Saved to %SUPPORT_HOME%\%ARCHIVE_NAME%"
+exit /B 0
+
+
+:logging
+    @rem BSR-1130
+    echo %~1
+    echo [%date%_%time%] %~1 >> %bsrsupport_log%
 exit /B 0
