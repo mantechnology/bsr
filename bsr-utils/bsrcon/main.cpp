89703fd7ce7a285d60ede136888947ae19bccefc
한성원 <swhan@mantech.co.kr>
2024-05-29 15:20:43 +0900

    BSR-1270 modified the path separator to be modified according to the operating system when changing the log storage path * modified not to create perfmon path when changing log save path

--- bsr-utils/bsrcon/main.cpp
+++ bsr-utils/bsrcon/main.cpp
@@ -930,13 +930,29 @@ int cmd_set_log_path(int *index, int argc, char* argv[])
 	(*index)++;
 
 	if (*index < argc) {
+		int i;
 #ifdef _WIN
 		char *ptr;
+
+		// BSR-1270
+		for (i = 0; i < strlen(argv[*index]); i++)
+			if (argv[*index][i] == '/')
+				argv[*index][i] = '\\';
+
 		newPath = strtok_s(argv[*index], "\"", &ptr);
 #else
+		// BSR-1270
+		for (i = 0; i < strlen(argv[*index]); i++)
+			if (argv[*index][i] == '\\')
+				argv[*index][i] = '/';
+
 		newPath = argv[*index];
 #endif
-		return set_log_path(newPath);
+		if(set_log_path(newPath))
+			printf("Failed to change log save path. %s\n", newPath);
+		else
+			printf("Change log save path. %s\n", newPath);
+
 	}
 	else
 		usage(false);
