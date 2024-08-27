f8151dc80ff813fa95773098d68dd56a5b69fe9b
한성원 <swhan@mantech.co.kr>
2024-08-27 11:43:36 +0900

    BSR-1376 change how function parameters are compared

--- bsr/bsr-kernel-compat/tests/have_blkdev_put_param_holder.c
+++ bsr/bsr-kernel-compat/tests/have_blkdev_put_param_holder.c
@@ -1,11 +1,13 @@
 #include <linux/blkdev.h>
 #include <linux/fs.h>
 
-void foo(void) {
-	struct block_device *bdev = NULL;
-	void *holder = NULL;
-	
-	void (*holder_blkdev_put)(struct block_device *, void *) = blkdev_put;
-	
-	holder_blkdev_put(bdev, holder);
+#ifndef __same_type
+# define __same_type(a, b) __builtin_types_compatible_p(typeof(a), typeof(b))
+#endif
+
+void holder_blkdev_put(struct block_device *bdev, void *holder);
+
+void foo(void)
+{
+    BUILD_BUG_ON(!(__same_type(blkdev_put, holder_blkdev_put)));
 }
