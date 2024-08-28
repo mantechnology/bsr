ac82ff74848587f034b7c78167a147f60d4c26da
한성원 <swhan@mantech.co.kr>
2024-08-28 09:53:22 +0900

    prepare 1.7.1.1

--- bsr/linux/bsr_config.h
+++ bsr/linux/bsr_config.h
@@ -32,7 +32,7 @@ extern const char *bsr_buildtag(void);
 
 /* End of external module for 2.6.33 stuff */
 
-#define TAG_VERSION "1.7.1.1(2024.8.27)"
+#define TAG_VERSION "1.7.1.1(2024.8.28)"
 #define REL_VERSION "1.7.1.1"
 #define PRO_VERSION_MIN 86
 // DW-1293 protocol version 112 starts to support fast invalidate(remote)
