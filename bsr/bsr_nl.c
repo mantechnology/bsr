abeba32f99023f824f46593ba59a138214b3183f
한성원 <swhan@mantech.co.kr>
2024-11-07 14:17:44 +0900

    BSR-1406 added missing put_ldev.

--- bsr/bsr_nl.c
+++ bsr/bsr_nl.c
@@ -5751,6 +5751,7 @@ int bsr_adm_invalidate_peer(struct sk_buff *skb, struct genl_info *info)
 	// BSR-1393
 	if (resource->node_opts.target_only) {
 		retcode = ERR_LOCAL_TARGET_ONLY;
+		put_ldev(__FUNCTION__, device);
 		goto out;
 	}
 
